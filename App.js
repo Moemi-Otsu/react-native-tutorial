cf8821ef2c9fb5e20333cf13e358d79ca0454a84
moemi ohtsu <s0623192@gmail.com>
2022-02-01 23:02:43 +0900

    ReactNativeコアコンポーネントのネスト

--- App.js
+++ App.js
@@ -1,28 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Image, ScrollView, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-const App = () => {
+const Cat = () => {
   return (
-    <ScrollView>
-      <View>
-        <Text>Some text</Text>
-        <Image
-          source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-          }}
-          style={{ width:200, height: 200 }}
-        />
-      </View>
+    <View>
+      <Text>Hello, I am...</Text>
       <TextInput
         style={{
           height: 40,
           borderColor: 'gray',
           borderWidth: 1
         }}
-        defaultValue='You can type in me'
+        defaultValue="Name me!"
       />
-    </ScrollView>
+    </View>
   );
 }
 
-export default App;
+export default Cat;
